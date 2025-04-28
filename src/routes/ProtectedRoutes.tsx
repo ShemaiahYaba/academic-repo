23f4445124aa8c8860506ddc534c608c5f420944
ShemaiahYaba <67932039+ShemaiahYaba@users.noreply.github.com>
2025-04-28 14:43:14 +0100

    refactor: update Post import path in ProtectedRoutes to correct directory structure

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -6,7 +6,7 @@ import { lazy } from "react";
 const Home = lazy(() => import("@/pages/Home"));
 const Journals = lazy(() => import("@/pages/Journals"));
 const Articles = lazy(() => import("@/pages/Articles"));
-const Post = lazy(() => import("@/pages/Post"));
+const Post = lazy(() => import("@/pages/Posts/Posts"));
 const EditorialTeam = lazy(() => import("@/pages/EditorialTeam"));
 const About = lazy(() => import("@/pages/About"));
 const JournalDetail = lazy(() => import("@/pages/Journal/JournalDetails"));

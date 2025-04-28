71bbf7f19793d17b8b71ea1bbfd08978bd13c0ce
ShemaiahYaba <67932039+ShemaiahYaba@users.noreply.github.com>
2025-04-28 12:40:52 +0100

    Refactor AppRoutes to separate protected and public routes
    - Introduced ProtectedRoutes and PublicRoutes components for better route management.
    - Simplified session check logic in useEffect for authentication state.
    - Updated rendering logic to conditionally display routes based on authentication status.

--- src/routes/Approutes.tsx
+++ src/routes/Approutes.tsx
@@ -4,24 +4,16 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthProvider";
 import { supabase } from "@/supabaseClient";
 import Preloader from "@/components/Preloader";
+import ProtectedRoutes from "@/routes/ProtectedRoutes";
+import PublicRoutes from "@/routes/PublicRoutes";
 
 const AppRoutes = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
-    supabase.auth
-      .getSession()
-      .then(
-        ({
-          data,
-        }: {
-          data: { session: import("@supabase/supabase-js").Session | null };
-        }) => {
-          const session: import("@supabase/supabase-js").Session | null =
-            data.session;
-          setIsAuthenticated(!!session);
-        }
-      );
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setIsAuthenticated(!!session);
+    });
 
     const { data: listener } = supabase.auth.onAuthStateChange(
       (
@@ -43,6 +35,7 @@ const AppRoutes = () => {
       <Suspense fallback={<Preloader />}>
         <Routes>
           <>
+            {isAuthenticated ? <ProtectedRoutes /> : <PublicRoutes />}
             <Route
               path="*"
               element={<Navigate to={isAuthenticated ? "/" : "/"} />}
